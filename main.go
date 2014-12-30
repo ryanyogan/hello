d9003bb5ef67f21681b04391171a0542f343b159
Ryan Yogan <ryanyogan@gmail.com>
2014-12-29 20:41:02 -0600

    Created a small web server

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import "net/http"
+
 func main() {
-	println("Hello!")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
 }
